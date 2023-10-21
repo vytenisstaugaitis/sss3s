2744d65c3b6eba08314a929ca4cd47fe38409e47
vytenisstaugaitis <30520456+vytenisstaugaitis@users.noreply.github.com>
2023-10-21 23:47:39 +0300

    added: tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
-<nav>
+<script>
+    import "../app.scss";
+</script>
+
+<nav class="px-10 pt-10 pb-5 flex gap-5">
     <a href="/">Home</a>
     <a href="/blog">Blog</a>
     <a href="/about">About</a>
 </nav>
 
-<slot/>
+<main class="px-10">
+    <slot/>
+</main>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,4 @@
 <h1>About</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget dui quis dolor gravida sollicitudin at et odio. Donec nunc ante, semper eget ipsum at, posuere placerat lorem. In hac habitasse platea dictumst. Proin in lectus accumsan, ultrices odio sed, feugiat est. Nulla nec nulla arcu. Aenean pulvinar nisi purus, at tincidunt ipsum maximus sed. Sed suscipit, justo sit amet sollicitudin consectetur, nunc velit venenatis velit, eget tempus felis erat eget nisl. Nam ac sapien bibendum, sagittis dolor vitae, dictum ex.</p>
+<div class="w-6/12 mt-5">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget dui quis dolor gravida sollicitudin at et odio. Donec nunc ante, semper eget ipsum at, posuere placerat lorem. In hac habitasse platea dictumst. Proin in lectus accumsan, ultrices odio sed, feugiat est. Nulla nec nulla arcu. Aenean pulvinar nisi purus, at tincidunt ipsum maximus sed. Sed suscipit, justo sit amet sollicitudin consectetur, nunc velit venenatis velit, eget tempus felis erat eget nisl. Nam ac sapien bibendum, sagittis dolor vitae, dictum ex.</p>
+</div>
