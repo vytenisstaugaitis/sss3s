a4cd198c02dc93c36cb786ed8f422a3536473fc4
vytenisstaugaitis <30520456+vytenisstaugaitis@users.noreply.github.com>
2023-10-22 11:15:06 +0300

    added: about text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Welcome</h1>
 <div class="mt-3 lg:mt-5">
-    <pre class="">         /           /   
+    <pre class="text-xs lg:text-sm">         /           /   
         /' .,,,,  ./       
        /';'     ,/        
       / /   ,,//,`'`     
@@ -20,5 +20,5 @@
    :   \         ;
       .
     </pre>
-    <p class="text-xs">Ooyamaneko</p>
+    <p class="text-xs -translate-y-10 translate-x-36">Ooyamaneko</p>
 </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,4 @@
 <h1>About</h1>
 <div class="lg:w-6/12 mt-3 lg:mt-5">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget dui quis dolor gravida sollicitudin at et odio. Donec nunc ante, semper eget ipsum at, posuere placerat lorem. In hac habitasse platea dictumst. Proin in lectus accumsan, ultrices odio sed, feugiat est. Nulla nec nulla arcu. Aenean pulvinar nisi purus, at tincidunt ipsum maximus sed. Sed suscipit, justo sit amet sollicitudin consectetur, nunc velit venenatis velit, eget tempus felis erat eget nisl. Nam ac sapien bibendum, sagittis dolor vitae, dictum ex.</p>
+    <p>Experiments with SvelteKit and interesting tools & libraries on the server.</p>
 </div>
