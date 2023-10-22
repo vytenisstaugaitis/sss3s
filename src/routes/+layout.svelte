10eccd00c38642a75bd9f868394fcec18075d43d
vytenisstaugaitis <30520456+vytenisstaugaitis@users.noreply.github.com>
2023-10-22 12:22:52 +0300

    fix: minor styling and code adjustments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,12 @@
     import { toggleMode } from '$lib/utils'
 </script>
 
-<nav class="px-5 lg:px-10 pt-5 lg:pt-10 pb-5 flex gap-5 lg:inline-flex items-center">
+<nav class="px-5 lg:px-10 pt-5 lg:pt-10 pb-7 flex gap-5 lg:inline-flex items-center">
     <a href="/">Home</a>
     <a href="/about">About</a>
     <a href="/blog">Blog</a>
-    
-    <button id="theme-toggle" class="ml-auto" on:click={toggleMode}><svg id="moon" xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 24" fill="none" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path></svg><svg id="sun" xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 24" fill="none" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg></button>
+
+    <button id="theme-toggle" class="ml-auto" on:click={toggleMode}><svg id="moon" xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 24" fill="none" stroke="var(--text)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path></svg><svg id="sun" xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 24" fill="none" stroke="var(--text)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg></button>
 </nav>
 
 <main class="px-5 lg:px-10">
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,4 @@
 <h1>About</h1>
 <div class="lg:w-6/12 mt-3 lg:mt-5">
-    <p>Experiments with SvelteKit and interesting tools & libraries on the server.</p>
+    <p>Experiments with SvelteKit on the server.</p>
 </div>
