6ee1a245c86e4eb222e162057d8b259efc54a6a4
vytenisstaugaitis <30520456+vytenisstaugaitis@users.noreply.github.com>
2023-10-23 01:41:49 +0300

    added: edit post on github link

--- src/routes/md-posts/[slug]/+page.svelte
+++ src/routes/md-posts/[slug]/+page.svelte
@@ -25,4 +25,14 @@
 	<div class="mt-5 lg:mt-8 content">
 		<svelte:component this={data.content} />
 	</div>
+
+	<div class="mt-8">
+		<a href="https://github.com/vytenisstaugaitis/sss3s/edit/main/src/posts/{data.slug}.md" class="flex gap-1.5 items-center text-sm underline" target="_blank">
+			<svg class="w-4" width="50" height="50" viewBox="0 0 24 24" stroke="var(--text)" fill="none">
+				<path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path>
+				<polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon>
+			</svg>
+			Edit this post on GitHub
+		</a>
+	</div>
 </article>
