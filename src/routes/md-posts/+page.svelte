f7cdfec9f0781ccfacfe5d55bc66e16b17245f54
vytenisstaugaitis <30520456+vytenisstaugaitis@users.noreply.github.com>
2023-10-23 01:50:33 +0300

    added: tailwind dark mode  selector

--- src/routes/md-posts/+page.svelte
+++ src/routes/md-posts/+page.svelte
@@ -8,7 +8,7 @@
 <h1>Posts</h1>
 <div class="mt-3 lg:mt-5 flex gap-3">
 	{#each data.posts as post}
-		<a href={$page.route.id+'/'+post.slug} alt="" class="p-4 border border-gray-700 rounded-lg lg:hover:bg-[rgba(255,255,255,0.01)]">
+		<a href={$page.route.id+'/'+post.slug} alt="" class="p-4 border border-gray-400 dark:border-gray-700 rounded-lg lg:hover:bg-[rgba(0,0,0,0.01)] dark:lg:hover:bg-[rgba(255,255,255,0.01)]">
 			<h6>{post.title}</h6>
 			<p class="mt-1 text-xs lg:text-sm">{formatDate(post.date)}</p>
 			<p class="mt-3 text-sm">{post.description}</p>
--- src/routes/md-posts/[slug]/+page.svelte
+++ src/routes/md-posts/[slug]/+page.svelte
@@ -27,7 +27,7 @@
 	</div>
 
 	<div class="mt-8">
-		<a href="https://github.com/vytenisstaugaitis/sss3s/edit/main/src/posts/{data.slug}.md" class="flex gap-1.5 items-center text-sm underline" target="_blank">
+		<a href="https://github.com/vytenisstaugaitis/sss3s/edit/main/src/posts/{data.slug}.md" class="inline-flex gap-1.5 items-center text-sm underline" target="_blank">
 			<svg class="w-4" width="50" height="50" viewBox="0 0 24 24" stroke="var(--text)" fill="none">
 				<path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path>
 				<polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon>
