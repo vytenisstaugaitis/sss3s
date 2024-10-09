c34664dbfefd2727b7f21dd28a4b3d35a764a80d
vytenisstaugaitis <30520456+vytenisstaugaitis@users.noreply.github.com>
2024-10-09 20:25:52 +0300

    fix: md posts path

--- src/routes/tests/+page.svelte
+++ src/routes/tests/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Tests</h1>
 <div class="mt-3 lg:mt-5">
     <ul class="list-inside list-[circle]">
-        <li><a href="./md-posts">Markdown Posts</a></li>
+        <li><a href="/md-posts">Markdown Posts</a></li>
     </ul>
 </div>
